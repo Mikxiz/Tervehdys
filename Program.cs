05beeca51d6430141657d731c46a37305bd65468
Mikxiz <38124633+Mikxiz@users.noreply.github.com>
2018-09-13 10:52:38 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Tervehdys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime tänään = DateTime.Now; // Tätä käytetään otsikossa
+            DateTime alkuhetki = DateTime.Now; //aikaleima ennen tietojen syöttöä
+            DateTime loppuhetki;
+            Double aikaero;
+            string asiakassegmentti;
+            CultureInfo culture = new CultureInfo("fi-FI"); //käytä aina fi-FI -muotoa (CultureInfoa)
+            string nimi;
+            string osoite;
+            string postitoimipaikka;
+            string sähköposti;
+            int ika;
+            int postinumero;
+            int puhelinnumero;
+            Console.Write("Tänään on: " + tänään.ToString("F", culture));
+            Console.WriteLine(" Tervetuloa hienon konsoliaplikaation käyttäjiksi!"); // Tervehdys teksti
+            Console.WriteLine();
+            Console.Write("Kerro nimesi: "); //Write pysyy samalla rivillä
+            //Console.WriteLine("Kerro nimesi: "); // WriteLine vaihtaa riviä
+            nimi = Console.ReadLine();
+            Console.Write("Kerro ikäsi: ");
+            ika = int.Parse(Console.ReadLine());
+            if (ika <7)
+            {
+                asiakassegmentti = "Alle kouluikäinen";
+            }
+            else if (ika >= 7 && ika < 17)
+            {
+                asiakassegmentti = "Lapsi tai varkki";
+            }
+            else if (ika >= 17 && ika < 24)
+            {
+                asiakassegmentti = "Nuori";
+            }
+            else if (ika >= 24 && ika < 66)
+            {
+                asiakassegmentti = "Keski-ikäinen";
+            }
+            else
+            {
+                asiakassegmentti = "Eläkeläinen - todennäköisesti";
+            }
+            Console.Write("Kerro osoitteesi: ");
+            osoite = Console.ReadLine();
+            Console.Write("Kerro postinumerosi: ");
+            postinumero = int.Parse(Console.ReadLine());
+            Console.Write("Kerro postitoimipaikkasi: ");
+            postitoimipaikka = Console.ReadLine();
+            Console.Write("Kerro puhelinnumerosi: ");
+            puhelinnumero = int.Parse(Console.ReadLine());
+            Console.Write("Kerro sähköpostisi: ");
+            sähköposti = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Hyvää päivää, " + nimi + ", ikäsi on: " + ika.ToString());
+            Console.WriteLine("Asut osoitteessa: " + osoite + ", " + postinumero.ToString() + ", " + postitoimipaikka);
+            Console.WriteLine("Puhelinnumerosi on: " + puhelinnumero + " ja sähköpostisi on: " + sähköposti);
+            // Miksi postinumerossa/puhelinnumerossa ei näy nolla ensimmäisenä numerona? Esim. 06100 näkyy konsolissa kirjoittaessa 6100
+            loppuhetki = DateTime.Now; //aikaleima tietojen syötön jälkeen
+            aikaero = loppuhetki.Subtract(alkuhetki).TotalSeconds;
+            Console.WriteLine("");
+            Console.WriteLine("Asiakasluokitus: " + asiakassegmentti);
+            Console.WriteLine("");
+            Console.WriteLine("Aikaa kului " + aikaero.ToString() + " Paina Enteriä lopettaaksesi ohjelman"); // Lopetus ohjeistus
+            Console.ReadLine();
+        }
+    }
+}
